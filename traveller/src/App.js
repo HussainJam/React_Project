b6844cec0cb4ee222d3f53933f1ced80b2308a9e
HussainJam <66440388+HussainJam@users.noreply.github.com>
2021-07-11 13:26:47 +0500

    front end completed

---
+++ traveller/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NavbarMain from "./components/Navbar";
+import Home from "./pages/Home";
+import Tour from "./pages/Tour";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavbarMain />
+        <Router>
+          <Home path="/" />
+          <Tour path="tour" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
