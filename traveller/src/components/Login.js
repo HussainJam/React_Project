6cc42edc80342c83e2411d18400738bcfc812119
HussainJam <66440388+HussainJam@users.noreply.github.com>
2021-07-11 18:47:23 +0500

    login page

--- traveller/src/components/Login.js
+++ traveller/src/components/Login.js
@@ -1,11 +1,29 @@
 import React from "react";
+
 const Login =()=>
 {
     return(
         <div>
-            <div className="display-1">
-            Login Page
+            
+            <div className="container"  >
+            <div className="row" >
+                <div className="col-md-4 offset-4" >
+                    <form className="form bg-secondary rounded-3 mt-5" >
+                        <div className="form-group d-grid gap-1 col-9 py-3 mx-auto">
+                            <h3 className="head">Please Login</h3>
+                            <input type="text" className="mail mb-2 form-control-lg text-muted" placeholder="Email Address" />
+                            <input type="password" className="pass mb-2 form-control-lg text-muted" placeholder="Password" />
+                            <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="cb" />
+                            <label for="cb" className="form-check-label">Remember Me</label>
+                            </div>
+                            <input type="button" className="btn-info mt-2 mb-3 text-white rounded-3 "  name="btn" value="Login" />
+                        </div>    
+                    </form>
+                </div>
             </div>
+        </div>
+            
         
         </div>
     )
